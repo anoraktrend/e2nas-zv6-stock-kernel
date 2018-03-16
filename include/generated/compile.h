e7a44ce449d8c42e7afdfc903841fffd73ae86ac
anoraktrend <trente.artist@gmailcom>
2018-03-16 18:47:05 -0400

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Mon Feb 9 10:01:41 KST 2015"
+#define LINUX_COMPILE_BY "amiko84.kang"
+#define LINUX_COMPILE_HOST "LGEARND11B3"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
